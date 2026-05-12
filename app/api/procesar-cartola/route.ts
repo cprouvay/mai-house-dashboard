ad71497553557d5a588313ceea2bb702b6edd0ca
cprouvay <cprouvay@gmail.com>
2026-05-12 11:22:08 -0400

    Update route.ts

--- app/api/procesar-cartola/route.ts
+++ app/api/procesar-cartola/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/lib/supabase'
-import XLSX from 'xlsx'
+import * as XLSX from 'xlsx'
 
 const supabase = createClient()
 
